03cd70e1629e630eb46aa8f3c11098bc81e86c7d
rozanuriln <rozasyamsidduha@gmail.com>
2022-07-07 02:04:33 +0700

    shamo-1

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\API\ProductCController;
+use App\Http\Controllers\API\ProductController;
+use App\Http\Controllers\API\ProductCategoryController;
+use App\Http\Controllers\API\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('products', [ProductController::class, 'all']);
+Route::get('categories', [ProductCategoryController::class, 'all']);
+
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    route::get('user', [UserController::class, 'fetch']);
+});
+
